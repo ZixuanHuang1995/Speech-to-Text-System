61109a33939bb566758bc736666dae881421d321
kenhuang1995 <zixuanh.tw@gmail.com>
2025-04-22 23:11:22 +0800

    Implement ModelManager for model handling and update QtMain UI with model selection features

---
+++ src/ModelManager.py
@@ -0,0 +1,54 @@
+import os
+import whisper
+import huggingface_hub as hf_hub
+import openvino_genai as ov_genai
+
+class ModelManager:
+    
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def get_model_list() -> list:
+        print("Get model list ...")
+        whisper_models = ['tiny.en', 'tiny', 'base']
+        openvino_whisper_models = [
+            "OpenVINO/whisper-tiny-fp16-ov",
+            "OpenVINO/whisper-base-fp16-ov",
+            "OpenVINO/distil-large-v3-int8-ov",
+        ]
+        return whisper_models + openvino_whisper_models
+    
+    def is_model_available(self, model_name) -> bool:
+        # Check if the model is available in the local directory
+        return os.path.exists(f"models/{model_name}")
+    
+    def download_model(self, model_list): 
+        for model_name in model_list:
+            print("=====" + model_name + "=====")
+            if not self.is_model_available(str(model_name)):
+                if model_name.startswith("OpenVINO/"):
+                    print(f"Download OpenVINO Whisper Model：{model_name} ...")
+                    model_id = model_name # "OpenVINO/whisper-tiny-fp16-ov"
+                    model_path = "models/" + model_name
+                    hf_hub.snapshot_download(model_id, local_dir=model_path)
+                else:
+                    print(f"Download Whisper Model：{model_name} ...")
+                    from whisper import _download, _MODELS
+                    _download(_MODELS[model_name], "models/", False)
+            else:
+                print(f"{model_name} already exists in the local directory.")
+        print(f"Model {model_id} downloaded and saved to {model_path}")
+
+    def load_model(self, model_name):
+        model_path = "models/" + model_name
+        if model_name.startswith("OpenVINO/"):
+            pipe = ov_genai.WhisperPipeline(model_path, "CPU")
+            return pipe
+        else:
+            model = whisper.load_model(model_path, "cpu")
+            model.eval()
+            return model
+
+
+
--- src/ui/QtMain.py
+++ src/ui/QtMain.py
@@ -1,52 +1,87 @@
-import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6 import QtCore, QtWidgets
+
+from src.ModelManager import ModelManager
 
 class MyWidget(QtWidgets.QWidget):
+    
+    model_name = None
+    model_list = None
+    model_manager = None
+
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.buttonModelOption = QtWidgets.QComboBox()
-        self.buttonModelOption.addItems(["tiny", "base", "large"])
-        self.buttonModelOption.setMaximumWidth(300)
-        self.buttonSelectFile = QtWidgets.QPushButton("Select File")
-        self.buttonMicrophone = QtWidgets.QPushButton("Microphone On/Off")
-        self.buttonSaveOutputs = QtWidgets.QPushButton("Save File")
-
-        self.checkboxAuto = QtWidgets.QCheckBox("Auto")
-        self.checkboxDetection = QtWidgets.QCheckBox("Detection")
-
+        self.create_menu()
+        self.create_horizontal_group_box()
+        self.create_grid_group_box()
         self.textOutputs = QtWidgets.QLabel("Output Text Box",
                                      alignment=QtCore.Qt.AlignCenter)
         self.textStatus = QtWidgets.QLabel("Status Bar",
                                      alignment=QtCore.Qt.AlignCenter)
-        
-        widgets = [self.buttonModelOption, self.buttonSelectFile,
-            self.buttonMicrophone, self.buttonSaveOutputs,
-            self.checkboxAuto, self.checkboxDetection,
-            self.textOutputs, self.textStatus]
-        
-        # for widget in widgets:
-            # widget.setMinimumHeight(40)
-            # widget.setMaximumWidth(200)
-            # self.layout.addWidget(widget)
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.buttonModelOption)
-        self.layout.addWidget(self.buttonSelectFile)
-        self.layout.addWidget(self.buttonMicrophone)
-        self.layout.addWidget(self.buttonSaveOutputs)
-        self.layout.addWidget(self.checkboxAuto)
-        self.layout.addWidget(self.checkboxDetection)
-        self.layout.addWidget(self.textOutputs)
-        self.layout.addWidget(self.textStatus)
-        self.layout.addStretch()
-        
-        self.buttonSelectFile.clicked.connect(self.magic)
+        main_layout = QtWidgets.QVBoxLayout()
+        main_layout.setMenuBar(self._menu_bar)
+        main_layout.addWidget(self._horizontal_group_box)
+        main_layout.addWidget(self._grid_group_box)
+        main_layout.addWidget(self.textOutputs)
+        main_layout.addWidget(self.textStatus)
+        self.setLayout(main_layout)
 
+        self.setWindowTitle("Qt Main Window")
+
+    def create_menu(self):
+        self._menu_bar = QtWidgets.QMenuBar()
+
+        self._file_menu = QtWidgets.QMenu("&File", self)
+        self._exit_action = self._file_menu.addAction("E&xit")
+        self._menu_bar.addMenu(self._file_menu)
+
+    def create_horizontal_group_box(self):
+        self._horizontal_group_box = QtWidgets.QGroupBox("Basic Components")
+        layout = QtWidgets.QHBoxLayout()
+        
+        model_manager = ModelManager()
+        model_list = model_manager.get_model_list()
+        buttonModelOption = QtWidgets.QComboBox()
+        buttonModelOption.addItems(model_list)
+        print("Model list: ", model_list, "done")
+        # buttonModelOption.setMaximumWidth(300)
+        layout.addWidget(buttonModelOption)
+        
+        buttonSelectFile = QtWidgets.QPushButton("Select File")
+        layout.addWidget(buttonSelectFile)
+        
+        buttonMicrophone = QtWidgets.QPushButton("Microphone On/Off")
+        layout.addWidget(buttonMicrophone)
+
+        self._horizontal_group_box.setLayout(layout)
+    
+    def create_grid_group_box(self):
+        self._grid_group_box = QtWidgets.QGroupBox("Advanced Controls")
+        layout = QtWidgets.QGridLayout()
+
+        checkboxAuto = QtWidgets.QCheckBox("Auto")
+        layout.addWidget(checkboxAuto, 1, 0)
+
+        checkboxDetection = QtWidgets.QCheckBox("Detection")
+        layout.addWidget(checkboxDetection, 2, 0)
+        
+        buttonSaveOutputs = QtWidgets.QPushButton("Save File")
+        layout.addWidget(buttonSaveOutputs, 1, 1)
+
+        buttonDownload = QtWidgets.QPushButton("Download Models")
+        layout.addWidget(buttonDownload, 2, 1)
+
+        layout.setColumnStretch(1, 20)
+        layout.setColumnStretch(2, 10)
+        self._grid_group_box.setLayout(layout)
+    
     @QtCore.Slot()
-    def magic(self):
-        self.textStatus.setText(random.choice(self.hello))
-
+    def download_model(self):
+        model_manager = ModelManager()
+        model_manager.download_model(self.model_list)
+    
+    @QtCore.Slot()
+    def select_file(self):
+        import os
+        
